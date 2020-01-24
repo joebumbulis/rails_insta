2b2c89269e0902d906e72809d8e69259b1f8aea9
joebumbulis <joebumbulis@gmail.com>
2020-01-24 15:36:33 -0600

    gets search functionality to work

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
+  get 'search/index'
   devise_for :users
   get 'home/index'
-
+  get 'search' => 'search#index'
+  
   resources :users, only: [:show, :edit, :update]
   resources :posts, only: [:new, :create, :show, :destroy]
 
